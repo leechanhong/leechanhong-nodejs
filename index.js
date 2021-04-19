f06fc1245c1b058be2ec59461ec4f80cfdc7e20b
kick3216@gmail.com <kick3216@gmail.com>
2021-04-19 10:50:36 +0900

    2교시 커밋 2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
+var app = express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
@@ -10,3 +10,9 @@ express()
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
   // 푸시기능, 차트 실시간 렌더링 하는 소켓앱 시작
+  //http서버 객체를 생성시 app(express) 프레임워크를 전달
+  var http = require('http').createServer(app);
+  //socket.io 소켓 통신 객체생성 시 http를 객체로 전달
+  var io = require('socket.io').listen(http);
+  //.listen은 클라이언트에서 서버로 접속을 받기 위해 대기하는 명령
+  http.listen(PORT, function(){});
